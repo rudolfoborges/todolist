bbb5ded16790830b7926593b7f02844291be5614
rudolfoborges <oliveira.rudolfo@gmail.com>
2014-11-02 22:14:09 -0300

    alteração na rotine de done

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,10 +3,9 @@ angular.module('todolist', [])
 .controller('TodoController', function($scope){
 
 	$scope.todos = [];
-	var localStorage = window.localStorage;
 
 	$scope.init = function(){
-		$scope.todos = angular.fromJson(localStorage.getItem('todos'));
+		$scope.todos = angular.fromJson(window.localStorage.getItem('todos'));
 		if($scope.todos === null){
 			$scope.todos = [];
 		}
@@ -14,20 +13,20 @@ angular.module('todolist', [])
 
 	$scope.add = function(){
 		$scope.todos.push({task: $scope.todo.task, done: false});
-		localStorage.setItem('todos', angular.toJson($scope.todos));
+		window.localStorage.setItem('todos', angular.toJson($scope.todos));
 		$scope.todo.task = '';
 	}
 
 	$scope.remove = function(index){
 		console.log(index);
 		$scope.todos.splice(index, 1);
-		localStorage.setItem('todos', angular.toJson($scope.todos));
+		window.localStorage.setItem('todos', angular.toJson($scope.todos));
 	}
 
 	$scope.done = function(index){
 		var todo = $scope.todos[index];
 		todo.done = true;
-		localStorage.setItem('todos', angular.toJson(todos));
+		window.localStorage.setItem('todos', angular.toJson(todos));
 	}
 
 });
